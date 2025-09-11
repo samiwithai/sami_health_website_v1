e1605ea363525d79799a171a2367384e79001624
Elie Masanka <masankaelie1@gmail.com>
2025-09-10 21:01:59 -0700

    another edit

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -27,7 +27,7 @@ export default function AboutSection() {
             SAMI+ is transforming prenatal care in the Democratic Republic of Congo, where maternal and newborn mortality rates remain among the highest in the world—with <strong>547</strong> maternal deaths per <strong>100,000</strong> live births, <strong>28</strong> newborn deaths per <strong>1,000</strong> live births, and <strong>28</strong> stillbirths per <strong>1,000</strong> births.
           </p>
           <p>
-            By training and deploying community health technicians equipped with portable, AI-powered ultrasound devices (POCUS), we are bridging critical diagnostic gaps, empowering providers with cutting-edge tools, strengthening local health systems, and bringing essential care directly to underserved rural communities. Our model enables early detection and timely clinical interventions—ensuring healthier pregnancies, safer births, and stronger beginnings, one scan at a time.
+            By training and deploying community health technicians equipped with portable, AI-powered point-of-care ultrasound (POCUS), we are bridging critical diagnostic gaps, empowering providers with cutting-edge tools, strengthening local health systems, and bringing essential care directly to underserved rural communities. Our model enables early detection and timely clinical interventions—ensuring healthier pregnancies, safer births, and stronger beginnings, one scan at a time.
           </p>
         </motion.div>
       </div>
