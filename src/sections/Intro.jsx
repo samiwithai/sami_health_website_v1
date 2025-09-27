236b4ba012a5a744cccb02ebea7bd9e0c7ef17f2
Elie Masanka <masankaelie1@gmail.com>
2025-09-26 19:13:15 -0700

    change intro text

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -24,10 +24,13 @@ export default function AboutSection() {
           transition={{ duration: 0.6 }}
         >
           <p>
-            SAMI+ is transforming prenatal care in the Democratic Republic of Congo, where maternal and newborn mortality rates remain among the highest in the world—with <strong>547</strong> maternal deaths per <strong>100,000</strong> live births, <strong>28</strong> newborn deaths per <strong>1,000</strong> live births, and <strong>28</strong> stillbirths per <strong>1,000</strong> births.
+            In the Democratic Republic of Congo (DRC), at least <strong>four</strong> women die every hour from
+            complications related to pregnancy or childbirth, and <strong>23</strong> newborns die each day due to birth-
+            related complications. Despite <strong>80%</strong> of births occurring in health facilities with trained
+            attendants, the DRC has the <strong>seventh-highest</strong> neonatal mortality rate globally and the <strong>third-highest</strong> in Africa.
           </p>
           <p>
-            By training and deploying community health technicians equipped with portable, AI-powered point-of-care ultrasound (POCUS), we are bridging critical diagnostic gaps, empowering providers with cutting-edge tools, strengthening local health systems, and bringing essential care directly to underserved rural communities. Our model enables early detection and timely clinical interventions—ensuring healthier pregnancies, safer births, and stronger beginnings, one scan at a time.
+            By training and deploying community health technicians equipped with portable, AI-powered point-of-care ultrasound (POCUS), <strong>SAMI+</strong> is bridging critical diagnostic gaps, empowering providers with cutting-edge tools, strengthening local health systems, and bringing essential care directly to underserved rural communities. Our model enables early detection and timely clinical interventions—ensuring healthier pregnancies, safer births, and stronger beginnings, one scan at a time.
           </p>
         </motion.div>
       </div>
